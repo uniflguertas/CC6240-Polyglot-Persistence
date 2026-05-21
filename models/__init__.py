01f5c2806ad8c667251fcafd2fe7cc4e15fded19
Luís Miguel <149299894+uniflguertas@users.noreply.github.com>
2026-05-21 00:01:32 -0300

    Delete models directory(ERRO NO COMMIT)

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.base import Base
-from app.models.customer import Customer
-from app.models.order import CompletedOrder, OrderItem
-
-__all__ = ["Base", "Customer", "CompletedOrder", "OrderItem"]
